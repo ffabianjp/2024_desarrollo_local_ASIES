d864d2a9768282ded1af7cb085c4b713304cd1e8
ana304sofia <ana304sofia@gmail.com>
2024-10-02 17:56:07 -0600

    DATA

--- R/Desarrollo_local/Scripts/Data analisis.R
+++ R/Desarrollo_local/Scripts/Data analisis.R
@@ -1,5 +1,7 @@
 #Desarrollo Local
-
+library(MatchIt)
+library(lmtest)
+library(sandwich)
 library(ggplot2)
 
 #abrir datos de FUNDESA.
@@ -32,3 +34,47 @@ summary(LR)
 
 LR=lm(tasa.global.de.participacion.mujeres ~ Turismo_pp, data =Datos_fundesa)
 summary(LR)
+
+# Propensity score matching
+match_obj <- matchit(Turistico ~ Poblacion + MIGRA_VIDA_pout + AGRICULTURA + Ladinos_porcentaje,
+                     data = Datos_fundesa, method = "nearest", distance ="glm",
+                     ratio = 1,
+                     replace = FALSE)
+summary(match_obj)
+
+
+plot(match_obj, type = "jitter", interactive = FALSE)
+plot(summary(match_obj), abs = FALSE)
+
+
+matched_data <- match.data(match_obj)
+
+res <- lm(ICL.2023 ~ Turistico, data = matched_data, weights = weights)
+#Test the coefficient using cluster robust standard error
+coeftest(res, vcov. = vcovCL, cluster = ~subclass)
+#Calculate the confidence intervals based on cluster robust standard error
+coefci(res, vcov. = vcovCL, cluster = ~subclass, level = 0.95)
+
+res <- lm(WES_SCORE_mean ~ Turistico, data = matched_data, weights = weights)
+#Test the coefficient using cluster robust standard error
+coeftest(res, vcov. = vcovCL, cluster = ~subclass)
+#Calculate the confidence intervals based on cluster robust standard error
+coefci(res, vcov. = vcovCL, cluster = ~subclass, level = 0.95)
+
+res <- lm(GINI ~ Turistico, data = matched_data, weights = weights)
+#Test the coefficient using cluster robust standard error
+coeftest(res, vcov. = vcovCL, cluster = ~subclass)
+#Calculate the confidence intervals based on cluster robust standard error
+coefci(res, vcov. = vcovCL, cluster = ~subclass, level = 0.95)
+
+res <- lm(tasa.global.de.participacion ~ Turistico, data = matched_data, weights = weights)
+#Test the coefficient using cluster robust standard error
+coeftest(res, vcov. = vcovCL, cluster = ~subclass)
+#Calculate the confidence intervals based on cluster robust standard error
+coefci(res, vcov. = vcovCL, cluster = ~subclass, level = 0.95)
+
+res <- lm(Pobreza ~ Turistico, data = matched_data, weights = weights)
+#Test the coefficient using cluster robust standard error
+coeftest(res, vcov. = vcovCL, cluster = ~subclass)
+#Calculate the confidence intervals based on cluster robust standard error
+coefci(res, vcov. = vcovCL, cluster = ~subclass, level = 0.95)
